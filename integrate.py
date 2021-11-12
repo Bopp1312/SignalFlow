bdaa95f9fa85d5b8f42a4e0fb488f72b99609b20
Bopp1312 <jonathanbopp@ucla.edu>
2021-11-11 17:10:29 -0800

    Added comments

--- integrate.py
+++ integrate.py
@@ -4,24 +4,30 @@ from maxent_irl import *
 def main():
     object = InterSpace()
     object.get_info()
-    #object.random_walk(10)
     object.states
 
+    # A demonstration of some trajectories
+    print("Walking in a square as a demonstration")
     object.square()
     traj = object.demonstrations
-    print(traj)
     feat_map = np.eye(object.n_states)
     mat = object.transition_mat
-    gamma = 0.9
-    iterations = 2
+
+    # Hyperparameters
+    gamma = 0.75
+    iterations = 20
     learning_rate = 0.5
     rewards = maxent_irl(feat_map, mat, gamma, traj, learning_rate, iterations)
     print(rewards)
-    values, policy = value_iteration.value_iteration(mat, rewards, gamma, error=0.01, deterministic=True)
-    print(policy)
-    print(np.shape(policy))
-    object.walk(policy,10,(3,0))
 
+    values, policy = value_iteration.value_iteration(mat, rewards, gamma, error=0.01, deterministic=True)
+    # Each cell in the policy corresponds to an action indicated by the id
+    print(policy)
+    # You can see that the policy is a array the size of the state space
+    print(np.shape(policy))
+
+    # Test out the trained policy to see what happens
+    object.walk(policy,10,(0,0))
 
 if __name__ == '__main__':
     main()
