c18d96115f2df6bb11be009e8b437cba5cbc4783
MuchachoAza <almakyns@gmail.com>
2021-11-18 15:17:27 -0800

    the dict issue solved

--- data/data_extraction.py
+++ data/data_extraction.py
@@ -2,10 +2,7 @@ import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from mdp.drawspace import DrawSpace
-import json
-
-def getting_data():
+def plotting_data(idx):
     mat = scipy.io.loadmat('mixoutALL_shifted.mat')
     letters = mat.get('mixout')[0]
 
@@ -13,50 +10,65 @@ def getting_data():
     for i in range(len(letters)):
         x = []
         y = []
-        x = [sum(letters[i][0][0:x:1]) for x in range(0, len(letters[i][0])+1)]#cummulattive sum
+        x = [sum(letters[i][0][0:x:1]) for x in range(0, len(letters[i][0])+1)]
         y = [sum(letters[i][1][0:x:1]) for x in range(0, len(letters[i][1])+1)]
-
-
+        #x = x[1:]
+        #y = y[1:]
         data.append([x,y])
-    plt.plot(data[70][0],data[70][1])
+    plt.plot(data[idx][0],data[idx][1])
     plt.show()
-    consts = list(mat.get('consts'))
-    idx = consts[0][0][4][0]
-    temp = consts[0][0][3][0]
-    keys = []
-    for i in list(temp):
-        keys.append(i[0])
-    #res = dict(zip(keys, list(d.values()))) # final dictinary with x-y cumulative distances and as values and letters as keys
+    return 
+
+def getting_rawdata():
+    mat = scipy.io.loadmat('mixoutALL_shifted.mat')
+    data = mat.get('mixout')[0]
     remapped_data = []
     for i in range(len(data)):
         circ_data = []
-        length = 0.0
-        angle = 0.0
-        for ii in range(len(data[i][0])-1):
-            deltaX = data[i][0][ii+1] - data[i][0][ii]
-            deltaY = data[i][1][ii+1] - data[i][1][ii]
-            delta_angle = math.atan2(deltaY, deltaX) #from x,y to l, theta
-            delta_length = math.sqrt(deltaX**2+deltaY**2)
-            length = delta_length + length
-            angle = delta_angle + angle
-            circ_data.append(((length, angle),(delta_length, delta_angle)))
-        #circ_data.append([math.sqrt((-data[i][0][-1])**2+(-data[i][1][-1])**2),math.atan2(-data[i][1][-1],data[i][0][-1])]) #adding 0-last point
+        for ii in range(len(data[i][0])):
+            deltaX = data[i][0][ii]
+            deltaY = data[i][1][ii]
+            angle = math.atan2(deltaY, deltaX)
+            length = math.sqrt(deltaX**2+deltaY**2)
+            circ_data.append([length,angle])
         remapped_data.append(circ_data)
-    index = []
+    return remapped_data
+    
+def getting_indexes(idx):
+    index_1h = []
+    index_2h = []
     items = list(range(1,21))
     for ii in items:
         for i in range(len(idx)-1,-1,-1):
             if idx[i] == ii:
-                index.append(i)
+                index_1h.append(i)
                 break
-    inf = 0
+
+    for ii in items:
+        for i in range(len(idx)-1):
+            if idx[i] == ii:
+                index_2h.append(i)
+                break
+    index_2h.append(index_1h[-1])
+    return index_2h
+
+def getting_data():
+    data = getting_rawdata()
+    index_dig = list(mat.get('consts')[0][0][4][0])
+    index_1 = getting_indexes(index_dig[0:1512])
+    index_2 = [x+(index_1[-1]+1) for x in getting_indexes(index_dig[index_1[-1]+1:])]
+    consts = list(mat.get('consts'))
+    temp_keys = consts[0][0][3][0]
+    keys = []
+    for i in list(temp_keys):
+        keys.append(i[0])
     letter_data = []
-    for i in index:
-        letter_data.append(remapped_data[inf:i+1])
-        inf = i+1
-    circ_dict = dict(zip(keys, letter_data))
-    return circ_dict #dict with letters and their new representation
-    #'a', 'b', 'c', 'd', 'e', 'g', 'h', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'y', 'z'
+    for i in range(len(index_1)-1):
+        temp = data[index_1[i]:index_1[i+1]]
+        temp.extend(data[index_2[i]:index_2[i+1]])
+        letter_data.append(temp)
+    circ_dict = dict(zip(keys,letter_data))
+    return circ_dict
 
 
 if __name__=='__main__':
